2d679a98d34f0ee8b083ffedae01b989a52215cb
dmarzoug <dries.marzougui@gmail.com>
2024-04-11 11:14:15 +0200

    [BrittleStar] Actuator strength parameter and adjustable solver iterations

--- biorobot/brittle_star/mjcf/morphology/specification/default.py
+++ biorobot/brittle_star/mjcf/morphology/specification/default.py
@@ -32,7 +32,7 @@ def default_joint_specification(range: float) -> BrittleStarJointSpecification:
 
 
 def default_arm_segment_specification(
-    alpha: float,
+        alpha: float,
 ) -> BrittleStarArmSegmentSpecification:
     in_plane_joint_specification = default_joint_specification(
         range=30 / 180 * np.pi
@@ -72,10 +72,11 @@ def default_arm_specification(num_segments_per_arm: int) -> BrittleStarArmSpecif
 
 
 def default_brittle_star_morphology_specification(
-    num_arms: int = 5,
-    num_segments_per_arm: Union[int, List[int]] = 5,
-    use_p_control: bool = False,
-    use_torque_control: bool = False,
+        num_arms: int = 5,
+        num_segments_per_arm: Union[int, List[int]] = 5,
+        use_p_control: bool = False,
+        use_torque_control: bool = False,
+        radius_to_strength_factor: float = 200
 ) -> BrittleStarMorphologySpecification:
     disk_specification = BrittleStarDiskSpecification(
         diameter=DISK_DIAMETER, height=DISK_HEIGHT
@@ -95,7 +96,8 @@ def default_brittle_star_morphology_specification(
         arm_specifications.append(arm_specification)
 
     actuation_specification = BrittleStarActuationSpecification(
-        use_p_control=use_p_control, use_torque_control=use_torque_control
+        use_p_control=use_p_control, use_torque_control=use_torque_control,
+        radius_to_strength_factor=radius_to_strength_factor
     )
     specification = BrittleStarMorphologySpecification(
         disk_specification=disk_specification,
