71e23ca9cf6844aad4f5a83280d7be8c872d651e
dmarzoug <dries.marzougui@gmail.com>
2024-09-19 18:49:10 +0200

    [BrittleStar] actuator force sensing temporary fix

--- biorobot/brittle_star/environment/shared/observables.py
+++ biorobot/brittle_star/environment/shared/observables.py
@@ -21,7 +21,7 @@ def get_quat2eueler_fn(backend: str) -> Callable[[chex.Array], chex.Array]:
 
 
 def get_num_contacts_and_segment_contacts_fn(
-    mj_model: mujoco.MjModel, backend: str
+        mj_model: mujoco.MjModel, backend: str
 ) -> Tuple[int, Callable[[BaseEnvState], chex.Array]]:
     if backend == "mjx":
         segment_capsule_geom_ids = np.array(
@@ -29,7 +29,7 @@ def get_num_contacts_and_segment_contacts_fn(
                 geom_id
                 for geom_id in range(mj_model.ngeom)
                 if "segment" in mj_model.geom(geom_id).name
-                and "capsule" in mj_model.geom(geom_id).name
+                   and "capsule" in mj_model.geom(geom_id).name
             ]
         )
 
@@ -39,8 +39,8 @@ def get_num_contacts_and_segment_contacts_fn(
 
             def solve_contact(geom_id: int) -> jnp.ndarray:
                 return (
-                    jnp.sum(contacts * jnp.any(geom_id == contact_data.geom, axis=-1))
-                    > 0
+                        jnp.sum(contacts * jnp.any(geom_id == contact_data.geom, axis=-1))
+                        > 0
                 ).astype(int)
 
             return jax.vmap(solve_contact)(segment_capsule_geom_ids)
@@ -77,15 +77,61 @@ def get_num_contacts_and_segment_contacts_fn(
     return num_contacts, get_segment_contacts
 
 
+def get_actuator_frc_fn(mj_model: mujoco.MjModel, backend: str) -> Callable[[BaseEnvState], chex.Array]:
+    # Temporary fix until https://github.com/google-deepmind/mujoco/issues/2068 is resolved
+    if backend == "mjx":
+        low_actuator_force_limit = jnp.array(
+            [limits[0] for limits in mj_model.actuator_forcerange]
+        )
+        high_actuator_force_limit = jnp.array(
+            [limits[1] for limits in mj_model.actuator_forcerange]
+        )
+
+        def calculate_actuator_force(state: MJXEnvState) -> jnp.ndarray:
+            l = state.mjx_data.actuator_length
+            v = state.mjx_data.actuator_velocity
+            gain = state.mjx_model.actuator_gainprm[:, 0]
+            b0 = state.mjx_model.actuator_biasprm[:, 0]
+            b1 = state.mjx_model.actuator_biasprm[:, 1]
+            b2 = state.mjx_model.actuator_biasprm[:, 2]
+            ctrl = state.mjx_data.ctrl
+
+            return jnp.clip(
+                gain * ctrl + b0 + b1 * l + b2 * v,
+                low_actuator_force_limit,
+                high_actuator_force_limit,
+            )
+
+        return calculate_actuator_force
+    else:
+        actuator_frc_sensors = [
+            mj_model.sensor(i)
+            for i in range(mj_model.nsensor)
+            if mj_model.sensor(i).type[0] == mujoco.mjtSensor.mjSENS_ACTUATORFRC
+        ]
+
+        def get_actuator_force(state: MJCEnvState) -> np.ndarray:
+            return np.array(
+                [
+                    state.mj_data.sensordata[sensor.adr[0]: sensor.adr[0] + sensor.dim[0]]
+                    for sensor in actuator_frc_sensors
+                ]
+            ).flatten()
+
+        return get_actuator_force
+
+
 def get_base_brittle_star_observables(
-    mj_model: mujoco.MjModel, backend: str
+        mj_model: mujoco.MjModel, backend: str
 ) -> List[BaseObservable]:
     if backend == "mjx":
         observable_class = MJXObservable
         bnp = jnp
+        get_data = lambda state: state.mjx_data
     else:
         observable_class = MJCObservable
         bnp = np
+        get_data = lambda state: state.mj_data
 
     sensors = [mj_model.sensor(i) for i in range(mj_model.nsensor)]
 
@@ -103,7 +149,7 @@ def get_base_brittle_star_observables(
         high=bnp.array([joint.range[1] for joint in joints]),
         retriever=lambda state: bnp.array(
             [
-                state.mj_data.sensordata[sensor.adr[0] : sensor.adr[0] + sensor.dim[0]]
+                get_data(state).sensordata[sensor.adr[0]: sensor.adr[0] + sensor.dim[0]]
                 for sensor in joint_pos_sensors
             ]
         ).flatten(),
@@ -115,13 +161,13 @@ def get_base_brittle_star_observables(
         for sensor in sensors
         if sensor.type[0] == mujoco.mjtSensor.mjSENS_JOINTVEL
     ]
-    joint_velocity_observable = MJCObservable(
+    joint_velocity_observable = observable_class(
         name="joint_velocity",
         low=-bnp.inf * bnp.ones(len(joint_vel_sensors)),
         high=bnp.inf * bnp.ones(len(joint_vel_sensors)),
         retriever=lambda state: bnp.array(
             [
-                state.mj_data.sensordata[sensor.adr[0] : sensor.adr[0] + sensor.dim[0]]
+                get_data(state).sensordata[sensor.adr[0]: sensor.adr[0] + sensor.dim[0]]
                 for sensor in joint_vel_sensors
             ]
         ).flatten(),
@@ -133,34 +179,24 @@ def get_base_brittle_star_observables(
         for sensor in sensors
         if sensor.type[0] == mujoco.mjtSensor.mjSENS_JOINTACTFRC
     ]
-    joint_actuator_force_observable = MJCObservable(
+    joint_actuator_force_observable = observable_class(
         name="joint_actuator_force",
         low=-bnp.inf * bnp.ones(len(joint_actuator_frc_sensors)),
         high=bnp.inf * bnp.ones(len(joint_actuator_frc_sensors)),
         retriever=lambda state: bnp.array(
             [
-                state.mj_data.sensordata[sensor.adr[0] : sensor.adr[0] + sensor.dim[0]]
+                get_data(state).sensordata[sensor.adr[0]: sensor.adr[0] + sensor.dim[0]]
                 for sensor in joint_actuator_frc_sensors
             ]
         ).flatten(),
     )
 
     # All actuator forces
-    actuator_frc_sensors = [
-        sensor
-        for sensor in sensors
-        if sensor.type[0] == mujoco.mjtSensor.mjSENS_ACTUATORFRC
-    ]
-    actuator_force_observable = MJCObservable(
+    actuator_force_observable = observable_class(
         name="actuator_force",
         low=bnp.array([limits[0] for limits in mj_model.actuator_forcerange]),
         high=bnp.array([limits[1] for limits in mj_model.actuator_forcerange]),
-        retriever=lambda state: bnp.array(
-            [
-                state.mj_data.sensordata[sensor.adr[0] : sensor.adr[0] + sensor.dim[0]]
-                for sensor in actuator_frc_sensors
-            ]
-        ).flatten(),
+        retriever=get_actuator_frc_fn(mj_model=mj_model, backend=backend),
     )
 
     # disk framepos
@@ -169,13 +205,13 @@ def get_base_brittle_star_observables(
         for sensor in sensors
         if sensor.type[0] == mujoco.mjtSensor.mjSENS_FRAMEPOS
     ][0]
-    disk_position_observable = MJCObservable(
+    disk_position_observable = observable_class(
         name="disk_position",
         low=-bnp.inf * bnp.ones(3),
         high=bnp.inf * bnp.ones(3),
         retriever=lambda state: bnp.array(
-            state.mj_data.sensordata[
-                framepos_sensor.adr[0] : framepos_sensor.adr[0] + framepos_sensor.dim[0]
+            get_data(state).sensordata[
+            framepos_sensor.adr[0]: framepos_sensor.adr[0] + framepos_sensor.dim[0]
             ]
         ),
     )
@@ -186,15 +222,15 @@ def get_base_brittle_star_observables(
         for sensor in sensors
         if sensor.type[0] == mujoco.mjtSensor.mjSENS_FRAMEQUAT
     ][0]
-    disk_rotation_observable = MJCObservable(
+    disk_rotation_observable = observable_class(
         name="disk_rotation",
         low=-bnp.pi * bnp.ones(3),
         high=bnp.pi * bnp.ones(3),
         retriever=lambda state: bnp.array(
             get_quat2eueler_fn(backend=backend)(
-                state.mj_data.sensordata[
-                    framequat_sensor.adr[0] : framequat_sensor.adr[0]
-                    + framequat_sensor.dim[0]
+                get_data(state).sensordata[
+                framequat_sensor.adr[0]: framequat_sensor.adr[0]
+                                         + framequat_sensor.dim[0]
                 ]
             )
         ),
@@ -206,14 +242,14 @@ def get_base_brittle_star_observables(
         for sensor in sensors
         if sensor.type[0] == mujoco.mjtSensor.mjSENS_FRAMELINVEL
     ][0]
-    disk_linvel_observable = MJCObservable(
+    disk_linvel_observable = observable_class(
         name="disk_linear_velocity",
         low=-bnp.inf * bnp.ones(3),
         high=bnp.inf * bnp.ones(3),
         retriever=lambda state: bnp.array(
-            state.mj_data.sensordata[
-                framelinvel_sensor.adr[0] : framelinvel_sensor.adr[0]
-                + framelinvel_sensor.dim[0]
+            get_data(state).sensordata[
+            framelinvel_sensor.adr[0]: framelinvel_sensor.adr[0]
+                                       + framelinvel_sensor.dim[0]
             ]
         ),
     )
@@ -224,14 +260,14 @@ def get_base_brittle_star_observables(
         for sensor in sensors
         if sensor.type[0] == mujoco.mjtSensor.mjSENS_FRAMEANGVEL
     ][0]
-    disk_angvel_observable = MJCObservable(
+    disk_angvel_observable = observable_class(
         name="disk_angular_velocity",
         low=-bnp.inf * bnp.ones(3),
         high=bnp.inf * bnp.ones(3),
         retriever=lambda state: bnp.array(
-            state.mj_data.sensordata[
-                frameangvel_sensor.adr[0] : frameangvel_sensor.adr[0]
-                + frameangvel_sensor.dim[0]
+            get_data(state).sensordata[
+            frameangvel_sensor.adr[0]: frameangvel_sensor.adr[0]
+                                       + frameangvel_sensor.dim[0]
             ]
         ),
     )
@@ -239,7 +275,7 @@ def get_base_brittle_star_observables(
     num_contacts, get_segment_contacts_fn = get_num_contacts_and_segment_contacts_fn(
         mj_model=mj_model, backend=backend
     )
-    segment_contact_observable = MJCObservable(
+    segment_contact_observable = observable_class(
         name="segment_contact",
         low=np.zeros(num_contacts),
         high=np.ones(num_contacts),
